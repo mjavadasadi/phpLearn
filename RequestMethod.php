3993f4e938349680f4a2fe47f39115569cbf5f5b
asadi74 <asadij313@gmail.com>
2023-09-07 11:15:01 +0330

    update

--- RequestMethod.php
+++ RequestMethod.php
@@ -1,15 +1,19 @@
 <?php
 var_dump($_GET);
+?><br>
+<?php
 var_dump($_POST);
+?><br>
+<?php
 var_dump($_REQUEST);
-?>
+?><br>
 
 <html>
 
 <head></head>
 
 <body>
-    <form action="RequestMethod.php/?site_name=mjavadasadi" method="post">
+    <form action="RequestMethod.php/?site_name=mjavadasadi.ir" method="post">
         <input type="text" name="Full_name"></input>
         <button type="submit">send</button>
     </form>
