b6423b1d95f12791ca96e6b568fc67129834f8c6
asadi74 <asadij313@gmail.com>
2023-09-07 10:49:53 +0330

    server method

---
+++ ServerMethod.php
@@ -0,0 +1,19 @@
+<html>
+
+<head></head>
+
+<body>
+
+    <?php var_dump($_SERVER) ?><br>
+    <?php var_dump($_SERVER["REMOTE_ADDR"]); ?><br>
+    <?php var_dump($_SERVER["HTTP_SEC_CH_UA_PLATFORM"]); ?><br>
+    <?php var_dump($_SERVER["SERVER_PROTOCOL"]); ?><br>
+    <?php var_dump($_SERVER["REQUEST_URI"]); ?><br>
+    <?php var_dump($_SERVER["HTTP_SEC_CH_UA_MOBILE"]); ?><br>
+    <?php var_dump($_SERVER["HTTP_USER_AGENT"]); ?><br>
+    <?php var_dump($_SERVER["REQUEST_TIME_FLOAT"]); ?><br>
+    <?php var_dump($_SERVER["HTTP_ACCEPT_LANGUAGE"]); ?><br>
+</body>
+
+
+</html>
